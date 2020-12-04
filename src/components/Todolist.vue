12b548fc813bd53e88fa78df3818db9fd1c05136
lisaguo-00 <56644642+lisaguo-00@users.noreply.github.com>
2020-12-04 16:24:05 +0800

    Initial commit

---
+++ src/components/Todolist.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <input
+      type="text"
+      class="todo-input"
+      placeholder="What needs to be done"
+      v-model="newTodo"
+      @keyup.enter="addTodo"
+    />
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item"></div>
+    <div>
+      <todo class="title"></todo>
+    </div>
+    <div class="remove item" @click="removeTodo(index)">&times;</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo-List",
+  data() {
+    return {
+      newTodo: "",
+      idforTodo: 3,
+      todos: [
+        {
+          id: 1,
+          title: "Finish Vue Screencast",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Take over world",
+          completed: false,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.idforTodo,
+        title: this.newTodo,
+        completed: false,
+      });
+
+      this.newTodo = "";
+      this.idforTodo++;
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+
+  &:focus {
+    outline: 0;
+  }
+}
+
+.todo-item {
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.remove-item {
+  cursor: pointer;
+  margin-left: 14px;
+  &:hover {
+    color: black;
+  }
+}
+</style>
